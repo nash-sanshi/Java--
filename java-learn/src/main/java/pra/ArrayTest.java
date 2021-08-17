5171ee538ddae498a9d0b6aa03c3f3fefd92b8e0
nash-sanshi <zhangleiabc@yeah.net>
2021-08-17 09:49:13 +0800

    默认变更列表

--- java-learn/src/main/java/pra/ArrayTest.java
+++ java-learn/src/main/java/pra/ArrayTest.java
@@ -2,14 +2,12 @@ package src.main.java.pra;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int []a = new int[100];
+    int [] a = new int[100];
         for (int i = 0; i < a.length; i++) {
             a[i] = i;
-
         }
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
-
     }
 }
